76911a6600e31591d451b7a7df79c81284b2bad8
priscrr <priscustodio19@gmail.com>
2025-04-04 01:24:48 +0000

    xadrez

--- xadrez.c
+++ xadrez.c
@@ -4,7 +4,7 @@ void moverBispo(int casas)
     for (int i = 0; i <= casas; i++);
     {
         printf("cima");
-        for (int j = 0; j <= i ; j++)
+        for (int j = 0; j <= j ; j++)
         {
             printf(", Direita");
             break;
